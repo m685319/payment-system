959f07a97a5b941fb8cfa8f031012701608ddf74
maria <m685319@yandex.ru>
2026-04-27 05:32:15 +0300

    feat(payment-service): initialize payment service with basic REST endpoint

---
+++ payment-service/src/main/java/dev/maria/payment/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package dev.maria.payment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    @PostMapping
+    public ResponseEntity<String> process() {
+        return ResponseEntity.ok("Payment processed");
+    }
+}
